3635a31f9e2ad9f9f11ad5b1098368454882f666
y-ytakahashi <tk.y_y.prive@icloud.com>
2023-09-01 11:00:27 +0900

    MUI Iconを追加

--- nextjs/discord-clone/components/sidebar/sidebar.tsx
+++ nextjs/discord-clone/components/sidebar/sidebar.tsx
@@ -1,28 +1,39 @@
 import React from "react";
 import styled from "styled-components";
+//MUI
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import AddIcon from '@mui/icons-material/Add';
 
 const SideBarMain = styled.div`
   display: flex;
   flex: 0.35;
   height: 100vh;
-  background-color: gray;
+  background-color:#32363D; //TODO あとでhomeに移動させる
 `;
 const SideBarLeft = styled.div`
   display: flex;
   flex-direction: column;
-  margin-top: 15px;
-  background-color: dimgray;
+  background-color: #1A1C20;
   padding: 7px 15px;
 ;
 `;
-const SideBarRight = styled.div``;
-const SideBarRightTop = styled.div``;
+const SideBarRight = styled.div`
+  background-color: #2B2D32;
+`;
+const SideBarRightTop = styled.div`
+  color: #FFF;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px;
+`;
 const ServerIcon = styled.div`
   width: 60px;
   height: 60px;
-  background-color: white;
+  background-color: #32363D;
   border-radius: 9999px;
   position: relative;
+  margin-top: 7px;
   
   img {
     position: absolute;
@@ -32,6 +43,22 @@ const ServerIcon = styled.div`
     transform: translate(-50%, -50%);
   }
 `;
+
+const SideBarChannels  = styled.div`
+  padding: 13px;
+`
+
+const SideBarHeader = styled.div`
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 5px;
+`
+
+const SideBarAddIcon = styled(AddIcon)`
+  cursor: pointer;
+`
 
 
 const Sidebar = () => {
@@ -50,7 +77,15 @@ const Sidebar = () => {
        sidebar right channel
        <SideBarRightTop>
          <h3>Discord</h3>
+         <ExpandMoreIcon />
        </SideBarRightTop>
+       <SideBarChannels>
+         <SideBarHeader>
+           <ExpandMoreIcon />
+           <h4>プログラミングチャンネル</h4>
+           <SideBarAddIcon />
+         </SideBarHeader>
+       </SideBarChannels>
      </SideBarRight>
    </SideBarMain>
   );
