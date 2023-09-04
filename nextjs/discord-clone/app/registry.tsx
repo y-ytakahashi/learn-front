d2290c48329b7fa75bcca05e30245bd9c07080fc
y-ytakahashi <tk.y_y.prive@icloud.com>
2023-09-04 14:37:16 +0900

    画面レンダリング時のエラー解決

--- nextjs/discord-clone/app/registry.tsx
+++ nextjs/discord-clone/app/registry.tsx
@@ -1,32 +1,31 @@
-'use client'
- 
-import React, { useState } from 'react'
-import { useServerInsertedHTML } from 'next/navigation'
-import { ServerStyleSheet, StyleSheetManager } from 'styled-components'
+"use client";
+
+import React, { useState } from "react";
+import { useServerInsertedHTML } from "next/navigation";
+import { ServerStyleSheet, StyleSheetManager } from "styled-components";
 import GlobalStyle from "@/app/styles/globalStyles";
- 
+
 export default function StyledComponentsRegistry({
-  children,
-}: {
+                                                   children
+                                                 }: {
   children: React.ReactNode
 }) {
   // Only create stylesheet once with lazy initial state
   // x-ref: https://reactjs.org/docs/hooks-reference.html#lazy-initial-state
-  const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet())
- 
+  const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet());
+
   useServerInsertedHTML(() => {
-    const styles = styledComponentsStyleSheet.getStyleElement()
-    styledComponentsStyleSheet.instance.clearTag()
-    return <>{styles}</>
-  })
- 
-  if (typeof window !== 'undefined') return <>{children}</>
- 
+    const styles = styledComponentsStyleSheet.getStyleElement();
+    styledComponentsStyleSheet.instance.clearTag();
+    return <>{styles}</>;
+  });
+
+  if (typeof window !== "undefined") return <>{children}</>;
+
   return (
     <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>
-      <GlobalStyle>
-        {children}
-      </GlobalStyle>
+      <GlobalStyle />
+      {children}
     </StyleSheetManager>
-  )
+  );
 }
