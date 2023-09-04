8df7c23d8acdf8cc3f6035bda2a603f11d9e8f0c
y-ytakahashi <tk.y_y.prive@icloud.com>
2023-09-04 15:39:36 +0900

    next-fontを使ってWebフォントを適用させる

--- nextjs/discord-clone/app/layout.tsx
+++ nextjs/discord-clone/app/layout.tsx
@@ -4,14 +4,19 @@ import React from "react";
 import GlobalStyles from "@/app/styles/globalStyles";
 import { ThemeProvider } from "styled-components";
 import theme from "@/app/styles/theme";
+import { Noto_Sans_JP } from "next/font/google";
+// NOTE Webfontの設定記事 https://zenn.dev/tsuyoshi/articles/894592ac677148
+const noteSansJapanese = Noto_Sans_JP({
+  preload: false
+});
 
 
 export default function RootLayout({ children }: {
   children: React.ReactNode;
 }) {
   return (
-    <html>
-    <body>
+    <html lang="en">
+    <body className={noteSansJapanese.className}>
     <StyledComponentsRegistry>
       <GlobalStyles />
       <ThemeProvider theme={theme}>
