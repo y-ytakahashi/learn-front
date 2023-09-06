89a1271c0d8e835d624e1824eabc22dd89f07a76
y-ytakahashi <tk.y_y.prive@icloud.com>
2023-09-06 13:46:19 +0900

    reduxToolkit導入と設定

--- nextjs/discord-clone/app/page.tsx
+++ nextjs/discord-clone/app/page.tsx
@@ -2,21 +2,33 @@
 import Sidebar from "@/components/sidebar/sidebar";
 import Chat from "@/components/chat/chat";
 import styled from "styled-components";
+import Login from "@/components/login/login";
+import { useAppSelector } from "@/app/hooks";
 
 
 const HomeContainer = styled.div`
   display: flex;
   flex-grow: 1;
-`
+`;
 
 
 export default function Home() {
+
+  const user = useAppSelector((state) => state.user);
+  console.log(user);
+
   return (
     <HomeContainer>
-      {/*  sidebar */}
-      <Sidebar />
-      {/* chat */}
-      <Chat />
+      {user ? (
+        <>
+          <Sidebar />
+          <Chat />
+        </>
+      ) : (
+        <Login />
+      )
+      }
     </HomeContainer>
+
   );
 }
