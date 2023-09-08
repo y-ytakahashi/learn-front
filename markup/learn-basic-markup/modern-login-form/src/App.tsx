95ce223aed8169bb7cd19075fd507f6a8dee189c
y-ytakahashi <tk.y_y.prive@icloud.com>
2023-09-08 17:01:14 +0900

    In the middle of implementing login form.

--- markup/learn-basic-markup/modern-login-form/src/App.tsx
+++ markup/learn-basic-markup/modern-login-form/src/App.tsx
@@ -1,13 +1,36 @@
-import './App.scss'
+import "./App.scss";
+import logoImage from "./assets/logo.png";
+import { Lock, Mail } from "@mui/icons-material";
 
 function App() {
-    return (
-        <>
-            <div className="container">
-                <h1>hello</h1>
+  return (
+    <>
+      <div className="login-card">
+        <div className="container">
+          <div className="logo">
+            <img src={logoImage} alt="logo" />
+          </div>
+          <div className="header">
+            <h1>Sign in</h1>
+            <div>Please login to use platform</div>
+          </div>
+          <form className="login-form">
+            <div className="form-item">
+              <Mail />
+              <input type={"text"} placeholder={"Email"} required={true} autoFocus={true} />
             </div>
-        </>
-    )
+            <div className="form-item">
+              <Lock />
+              <input type={"password"} placeholder={"Password"} required={true} />
+            </div>
+            <div className="form-item-other">
+              {/*  add check box element*/}
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
