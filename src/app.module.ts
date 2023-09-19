f18702a6afb64b04cad03f67cd8b226add468751
y-ytakahashi <tk.y_y.prive@icloud.com>
2023-09-19 14:31:27 +0900

    created auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
-import {Module} from '@nestjs/common';
-import {AppController} from './app.controller';
-import {AppService} from './app.service';
-import {AuthModule} from './auth/auth.module';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-    imports: [AuthModule],
-    controllers: [AppController],
-    providers: [AppService],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class AppModule {
-}
+export class AppModule {}
