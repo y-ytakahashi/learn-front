87d475743b2291c704aa390c5017922844dfb8f1
y-ytakahashi <tk.y_y.prive@icloud.com>
2023-09-20 14:06:01 +0900

    created navbar

---
+++ nextjs/basic-sns/sns-client/components/Navbar/index.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import styles from "./style.module.scss"
+
+const Navbar = () => {
+  return (
+    <header className={styles.container}>
+      <h3>Basic SNS</h3>
+      <nav>
+        <ul>
+          <li>
+            <Link href={"/login"}>Login</Link>
+          </li>
+          <li>
+            <Link href={"/signUp"}>SignUp</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+export default Navbar;
