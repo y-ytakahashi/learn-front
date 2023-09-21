cdb952cd21d17741d42410ca10551e14d5a5bd7f
y-ytakahashi <tk.y_y.prive@icloud.com>
2023-09-21 17:36:58 +0900

    Fixed a bug that the response does not return

--- nextjs/basic-sns/sns-api/src/middleware/token.middleware.ts
+++ nextjs/basic-sns/sns-api/src/middleware/token.middleware.ts
@@ -1,5 +1,6 @@
 import {
   Injectable,
+  InternalServerErrorException,
   NestMiddleware,
   UnauthorizedException,
 } from '@nestjs/common';
@@ -13,20 +14,25 @@ export class TokenMiddleware implements NestMiddleware {
   constructor(private jwtService: JwtService) {}
 
   async use(req: Request, res: Response, next: NextFunction) {
-    console.log('Middleware is called');
     const token = this.extractTokenFromHeader(req);
     if (!token) {
       throw new UnauthorizedException('Token is valid');
     }
+    console.log('Middleware is called', token);
+
     try {
-      const payload = this.jwtService.verifyAsync(token, {
+      console.log('start verify token');
+      const payload = await this.jwtService.verifyAsync(token, {
         secret: process.env.JWT_SECRET,
       });
-      console.log('Middle Ware', { payload });
+      console.log('middleware', { payload });
       // 💡 We're assigning the payload to the request object here
       // so that we can access it in our route handlers
       req['user'] = payload;
-    } catch (e) {}
+    } catch (e) {
+      throw new InternalServerErrorException(e.message);
+    }
+    next();
   }
 
   private extractTokenFromHeader(request: Request): string | undefined {
