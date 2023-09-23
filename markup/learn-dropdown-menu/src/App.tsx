a45dfad66a93b71c5e76bb4030db657e6a53a98f
y-ytakahashi <tk.y_y.prive@icloud.com>
2023-09-23 18:55:24 +0900

    Change navigation bar background according to scroll position

--- markup/learn-dropdown-menu/src/App.tsx
+++ markup/learn-dropdown-menu/src/App.tsx
@@ -1,10 +1,45 @@
 import styles from "./app.module.scss";
-import { useCallback, useState } from "react";
+import { useCallback, useState, useRef, useEffect } from "react";
 import clsx from "clsx";
 
+type ActiveNavBar = {
+  tagName: string;
+  isActive: boolean;
+}
 
 function App() {
   const [toggle, setToggle] = useState(false);
+  const homeRef = useRef<HTMLDivElement>(null);
+
+  const serviceRef = useRef<HTMLDivElement>(null);
+  const aboutRef = useRef<HTMLDivElement>(null);
+  const [homeY, setHomeY] = useState<number>(0);
+  const [serviceY, setServiceY] = useState<number>(0);
+  const [aboutY, setAboutY] = useState<number>(0);
+  const [navActive, setNavActive] = useState<ActiveNavBar>({ tagName: "", isActive: false });
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setHomeY(Math.abs(homeRef.current?.getBoundingClientRect().y || 0));
+      setServiceY(Math.abs(serviceRef.current?.getBoundingClientRect().y || 0));
+      setAboutY(aboutRef.current?.getBoundingClientRect().y || 0);
+
+      if (homeY < serviceY) {
+        setNavActive({ ...navActive, tagName: "Home", isActive: true });
+      } else if (serviceY < aboutY) {
+        setNavActive({ ...navActive, tagName: "Service", isActive: true });
+      } else {
+        setNavActive({ ...navActive, tagName: "About", isActive: true });
+      }
+
+    };
+
+
+    document.addEventListener("scroll", handleScroll, true);
+    return () => {
+      document.removeEventListener("scroll", handleScroll, true);
+    };
+  }, [homeY]);
 
   // Function to toggle navbar
   const toggleNavbar = useCallback(() => setToggle((prev) => !prev), []);
@@ -14,10 +49,10 @@ function App() {
     <div className={styles.container}>
       <nav className={styles.navbar}>
         <ul className={styles.navLinks}>
-          <li className={styles.navLink}>
+          <li className={clsx(styles.navLink, navActive.tagName === "Home" && styles.navActive)}>
             <a href={"#Home"}>Home</a>
           </li>
-          <li className={`${styles.navLink} ${styles.service}`}>
+          <li className={clsx(styles.navLink, styles.service, navActive.tagName === "Service" && styles.navActive)}>
             <a href={"#Service"}>Service</a>
             <ul className={styles.dropDown}>
               <li>Service 1</li>
@@ -25,7 +60,7 @@ function App() {
               <li>Service 3</li>
             </ul>
           </li>
-          <li className={styles.navLink}>
+          <li className={clsx(styles.navLink, navActive.tagName === "About" && styles.navActive)}>
             <a href={"#About"}>About</a>
           </li>
           <li className={styles.navLink}>
@@ -34,13 +69,18 @@ function App() {
         </ul>
       </nav>
       <main className={styles.mainContainer}>
-        <section id={"Home"} className={styles.secContainer}>
-          <h1>Home</h1>
+        <section ref={homeRef} id={"Home"} className={styles.secContainer}>
+          <div className={styles.bodyContainer}>
+            <h1>Home</h1>
+            <div>
+              <p>hogehoge</p>
+            </div>
+          </div>
         </section>
-        <section id={"Service"} className={styles.secContainer}>
+        <section ref={serviceRef} id={"Service"} className={styles.secContainer}>
           <h1>Service</h1>
         </section>
-        <section id={"About"} className={styles.secContainer}>
+        <section ref={aboutRef} id={"About"} className={styles.secContainer}>
           <h1>About</h1>
         </section>
         <div className={clsx(styles.content, toggle && styles.show)}>
