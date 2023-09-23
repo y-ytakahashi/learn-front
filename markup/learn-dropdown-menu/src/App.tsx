d6e132473160bc52e718e4929eaf3851bdd827a5
y-ytakahashi <tk.y_y.prive@icloud.com>
2023-09-23 16:35:20 +0900

    top navigation bar change position the left side

--- markup/learn-dropdown-menu/src/App.tsx
+++ markup/learn-dropdown-menu/src/App.tsx
@@ -15,10 +15,10 @@ function App() {
       <nav className={styles.navbar}>
         <ul className={styles.navLinks}>
           <li className={styles.navLink}>
-            <a href="#">Home</a>
+            <a href={"#Home"}>Home</a>
           </li>
           <li className={`${styles.navLink} ${styles.service}`}>
-            <a href="#">Service</a>
+            <a href={"#Service"}>Service</a>
             <ul className={styles.dropDown}>
               <li>Service 1</li>
               <li>Service 2</li>
@@ -26,19 +26,30 @@ function App() {
             </ul>
           </li>
           <li className={styles.navLink}>
-            <a href="#">Service</a>
+            <a href={"#About"}>About</a>
           </li>
           <li className={styles.navLink}>
-            <a href="#" onClick={toggleNavbar}>Contanct</a>
+            <a href={"#Contact"} onClick={toggleNavbar}>Contact</a>
           </li>
         </ul>
       </nav>
-      <div className={clsx(styles.content, toggle && styles.show)}>
-        <div>
-          <h1>menu</h1>
+      <main className={styles.mainContainer}>
+        <section id={"Home"} className={styles.secContainer}>
+          <h1>Home</h1>
+        </section>
+        <section id={"Service"} className={styles.secContainer}>
+          <h1>Service</h1>
+        </section>
+        <section id={"About"} className={styles.secContainer}>
+          <h1>About</h1>
+        </section>
+        <div className={clsx(styles.content, toggle && styles.show)}>
+          <div>
+            <h1>Contact</h1>
+          </div>
+          <div onClick={toggleNavbar}>x</div>
         </div>
-        <div onClick={toggleNavbar}>x</div>
-      </div>
+      </main>
     </div>
   );
 }
