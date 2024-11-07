848ce6bec6546367f34c48ca6bea7ec7af29e22a
y-ytakahashi <tk.y_y.prive@icloud.com>
2024-11-07 19:36:05 +0900

    mdxを表示させるところまで実装した

--- src/app/(marketing)/blog/page.tsx
+++ src/app/(marketing)/blog/page.tsx
@@ -5,10 +5,19 @@ export default function BlogPage() {
     <div className="container max-w-4xl py-6 lg:py-10">
       <div>
         <div className="space-y-4">
-          <h1 className="font-extrabold">Blogs</h1>
+          <h1 className="font-extrabold text-4xl lg:text-5xl tracking-tight">
+            Blogs
+          </h1>
           <p>content layer で作成しています</p>
         </div>
       </div>
+      <hr className="my-8" />
+      {allPosts.map((post) => (
+        <article key={post._id}>
+          <h2 className="text-2xl font-extrabold">{post.title}</h2>
+          <p>{post.body.raw}</p>
+        </article>
+      ))}
     </div>
   );
 }
